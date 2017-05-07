8c2c4fef411c8e11c4bcdeb4525538066e0e39c7
NORTHNRORO\Northnroro <northnroro@hotmail.com>
2017-05-07 19:52:40 +0800

    auto commit

--- Regex/index.js
+++ Regex/index.js
@@ -1,30 +1,45 @@
 $('body').show();
+$('.choice-item').hide();
+initInputField($('[data-question-id] .choice-row').css({
+	border: '1px #ffcfa0 solid',
+    background: '#ffe8cd'
+}));
 
 var data1 = "นาย ก,74\nนาย ข,99\nนาย ค,5\nนางสาว ง,47\nด.ญ. ฉ,100\n"
-var ans1 = "นาย ก\nนาย ข\nนาย ค\nนางสาว ง\nด.ญ. ฉ\n"
+var ans1 = "74\n99\n5\n47\n100\n"
 setData(data1, $('[data-question-id=1] .title'), ans1);
 
-function setData(data, afterEle, answer) {
-	$(afterEle).parent().children(':gt(' + $(afterEle).index() + ')').remove();
-	var codeBox = $('<fieldset>').css({
+function initInputField(divElement){
+	var inputFieldTemplate = $('<div>').css({
+		padding: '3px 5px'
+	}).append($('<input>'));
+	$(divElement).append(inputFieldTemplate.clone().prepend($('<label>').text("Find: ")));
+	$(divElement).append(inputFieldTemplate.clone().prepend($('<label>').text("Replace: ")));
+}
+
+function setData(data, afterElement, answer) {
+	$(afterElement).parent().children(':gt(' + $(afterElement).index() + ')').remove();
+	var codeBoxTemplate = $('<fieldset>').css({
 		'white-space': 'pre',
 		display: 'inline-block',
 		width: '33%',
 		border: '3px ridge rgb(255, 178, 102)',
-    	padding: '3px',
-    	margin: '0px'
+    	padding: '5px',
+    	margin: '0px',
+    	'margin-bottom': '-10px',
+    	background: '#fff7ef'
 	});
-	$(afterEle).after(codeBox.clone().html(answer).prepend($('<legend>Expected</legend>').css({
+	$(afterElement).after(codeBoxTemplate.clone().html(answer).prepend($('<legend>Expected</legend>').css({
 		padding: '0px 5px',
     	'margin-left': '5px'
 	})));
-	$(afterEle).after(codeBox.clone().html(data).prepend($('<legend>Output</legend>').css({
+	$(afterElement).after(codeBoxTemplate.clone().html(data).prepend($('<legend>Output</legend>').css({
 		padding: '0px 5px',
     	'margin-left': '5px'
 	})).css({
 		'border-width': '3px 0px'
 	}));
-	$(afterEle).after(codeBox.clone().html(data).prepend($('<legend>Input</legend>').css({
+	$(afterElement).after(codeBoxTemplate.clone().html(data).prepend($('<legend>Input</legend>').css({
 		padding: '0px 5px',
     	'margin-left': '5px'
 	})));
