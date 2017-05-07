9e66bc495600ac094d384ba9bd8ff35e5cc2de4e
NORTHNRORO\Northnroro <northnroro@hotmail.com>
2017-05-07 19:04:48 +0800

    auto commit

--- Regex/index.js
+++ Regex/index.js
@@ -1,28 +1,31 @@
 $('body').show();
 
-var str1 = "นาย ก,74\nนาย ข,99\nนาย ค,5\nนางสาว ง,47\nด.ญ. ฉ,100\n"
-setData(str1, $('[data-question-id=1] .title'));
+var data1 = "นาย ก,74\nนาย ข,99\nนาย ค,5\nนางสาว ง,47\nด.ญ. ฉ,100\n"
+var ans1 = "นาย ก\nนาย ข\nนาย ค\nนางสาว ง\nด.ญ. ฉ\n"
+setData(data1, $('[data-question-id=1] .title'), ans1);
 
-function setData(string, afterEle) {
+function setData(data, afterEle, answer) {
 	$(afterEle).parent().children(':gt(' + $(afterEle).index() + ')').remove();
 	var codeTemplate = $('<fieldset>').css({
 		'white-space': 'pre',
 		display: 'inline-block',
 		width: '33%',
-		border: '3px ridge rgb(255, 222, 113)',
+		border: '3px ridge rgb(255, 178, 102)',
     	padding: '3px',
     	margin: '0px'
-	}).html(string);
+	});
 	$(afterEle).after(codeTemplate.clone().prepend($('<legend>Expected</legend>').css({
 		padding: '0px 5px',
     	'margin-left': '5px'
-	})));
+	})).html(answer));
 	$(afterEle).after(codeTemplate.clone().prepend($('<legend>Output</legend>').css({
 		padding: '0px 5px',
     	'margin-left': '5px'
-	})));
+	})).css({
+		'border-width': '3px 0px'
+	}).html(data));
 	$(afterEle).after(codeTemplate.clone().prepend($('<legend>Input</legend>').css({
 		padding: '0px 5px',
     	'margin-left': '5px'
-	})));
+	})).html(data));
 }
