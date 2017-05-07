ad6655b450b43a557d971657239c3c4c68dd559f
NORTHNRORO\Northnroro <northnroro@hotmail.com>
2017-05-07 18:36:25 +0800

    auto commit

--- Regex/index.js
+++ Regex/index.js
@@ -1,4 +1,9 @@
 $('body').show();
 
 var str1 = "นาย ก,74\nนาย ข,99\nนาย ค,5\nนางสาว ง,47\nด.ญ. ฉ,100\n"
-$('[data-question-id=1] .title').after($('<div>').html(str1));
+setData(str1, $('[data-question-id=1] .title'));
+
+function setData(string, afterEle) {
+	$(afterEle).parent().children(':not(:first-child)').remove();
+	$(afterEle).after($('<div>').css('white-space', 'pre').html(string));
+}
