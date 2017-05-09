2729055e2f0c1c0d3aed078b54eeb3aa56324791
NORTHNRORO\Northnroro <northnroro@hotmail.com>
2017-05-10 01:19:29 +0800

    Add replace feature.

--- Regex/index.js
+++ Regex/index.js
@@ -19,27 +19,25 @@ function initInputField(divElements, bindQuestionTitles, dataStrings, answerStri
 	}).attr('placeholder', '(max. 50 characters)').attr('maxlength', 50));
 	for(var i in dataStrings){
 		var findDiv = inputFieldTemplate.clone();
-		$(divElements[i]).append(findDiv.prepend($('<label>').text("Find RegExp: ").css({width: '17%', display: 'inline-block'})).keyup((function(i,input){
-			return function(){
-				var result, string = dataStrings[i], matches = [], currPos = 0;;
-				while((result = string.match(new RegExp(input.children('input').val()))) && string.length > 0){
-					string = string.substring(result.index + Math.max(1, result[0].length));
-					matches.push({start: currPos + result.index, length: result[0].length});
-					currPos += result.index + Math.max(1, result[0].length);
-				}
-				setData(dataStrings[i], bindQuestionTitles[i], answerStrings[i], matches);
-			};
-		})(i,$(findDiv))));
+		$(divElements[i]).append(findDiv.prepend($('<label>').text("Find RegExp: ").css({width: '17%', display: 'inline-block'})).keyup(getChangeFunction(i,$(findDiv),$(replaceDiv),bindQuestionTitles,dataStrings,answerStrings)));
 		var replaceDiv = inputFieldTemplate.clone();
-		$(divElements[i]).append(replaceDiv.prepend($('<label>').text("Replace With: ").css({width: '17%', display: 'inline-block'})).keyup((function(i,input){
-			return function(){
-				console.log(answerStrings[i]);
-			};
-		})(i,$(replaceDiv))));
+		$(divElements[i]).append(replaceDiv.prepend($('<label>').text("Replace With: ").css({width: '17%', display: 'inline-block'})).keyup(getChangeFunction(i,$(findDiv),$(replaceDiv),bindQuestionTitles,dataStrings,answerStrings)));
 	}
 }
 
-function setData(data, afterElement, answer, hilight) {
+function getChangeFunction(i,find,replace,bindQuestionTitles,dataStrings,answerStrings){
+	return function(){
+		var result, string = dataStrings[i], matches = [], currPos = 0;;
+		while((result = string.match(new RegExp(find.children('input').val()))) && string.length > 0){
+			string = string.substring(result.index + Math.max(1, result[0].length));
+			matches.push({start: currPos + result.index, length: result[0].length});
+			currPos += result.index + Math.max(1, result[0].length);
+		}
+		setData(dataStrings[i], bindQuestionTitles[i], answerStrings[i], matches, find.children('input').val(), replace.children('input').val());
+	};
+}
+
+function setData(data, afterElement, answer, hilight, findRegex, replaceRegex) {
 	$(afterElement).parent().children(':gt(' + $(afterElement).index() + ')').remove();
 	var codeBoxTemplate = $('<fieldset>').css({
 		'white-space': 'pre',
@@ -51,32 +49,41 @@ function setData(data, afterElement, answer, hilight) {
     	'margin-bottom': '-10px',
     	background: '#fff7ef'
 	});
-	$(afterElement).after(codeBoxTemplate.clone().html(answer).prepend($('<legend>Expected</legend>').css({
-		padding: '0px 5px',
-    	'margin-left': '5px'
-	})));
-	$(afterElement).after(codeBoxTemplate.clone().html(data).prepend($('<legend>Output</legend>').css({
-		padding: '0px 5px',
-    	'margin-left': '5px'
-	})).css({
-		'border-width': '3px 0px'
-	}));
 	// Input + Hilight
-	var hilightedData = "", currPos = 0;
-	if(hilight){
+	var hilightedData = "", hilightedResult = "", currPos = 0;
+	if(hilight){ // For Data -> Hilight Only | For Ans -> Hilight and Replace
 		for(var i in hilight){
 			hilightedData += data.substring(currPos, hilight[i].start);
+			hilightedResult += data.substring(currPos, hilight[i].start);
 			hilightedData += $('<div>').append($('<span>').css({
 				background: '#ffd600',
 			    'box-shadow': 'inset 0px 0px 0px 1px #ff9c00',
 			    'border-radius': '3px'
 			}).text(data.substring(hilight[i].start, hilight[i].start + hilight[i].length))).html();
+			hilightedResult += $('<div>').append($('<span>').css({
+				background: '#ffd600',
+			    'box-shadow': 'inset 0px 0px 0px 1px #ff9c00',
+			    'border-radius': '3px'
+			}).text(data.substring(hilight[i].start, hilight[i].start + hilight[i].length).replace(new RegExp(findRegex), eval('\"' + (replaceRegex || '') + '\"')))).html();
 			currPos = hilight[i].start + hilight[i].length;
 		}
 		hilightedData += data.substring(currPos);
+		hilightedResult += data.substring(currPos);
 	}else{
 		hilightedData = data;
+		hilightedResult = data;
 	}
+	// Attach code box with html text
+	$(afterElement).after(codeBoxTemplate.clone().html(answer).prepend($('<legend>Expected</legend>').css({
+		padding: '0px 5px',
+    	'margin-left': '5px'
+	})));
+	$(afterElement).after(codeBoxTemplate.clone().html(hilightedResult).prepend($('<legend>Output</legend>').css({
+		padding: '0px 5px',
+    	'margin-left': '5px'
+	})).css({
+		'border-width': '3px 0px'
+	}));
 	$(afterElement).after(codeBoxTemplate.clone().html(hilightedData).prepend($('<legend>Input</legend>').css({
 		padding: '0px 5px',
     	'margin-left': '5px'
