a3fe2ead612d07fc7a26b8bbfcba3593270e7b25
NORTHNRORO\Northnroro <northnroro@hotmail.com>
2017-05-10 22:37:05 +0800

    Fix html find & replace bug.

--- Regex/index.js
+++ Regex/index.js
@@ -1,14 +1,30 @@
 $('body').show();
 $('.choice-item').hide();
 
-var data1 = "นาย ก,74\nนาย ข,99\nนาย ค,5\nนางสาว ง,47\nด.ญ. ฉ,100\n"
-var ans1 = "74\n99\n5\n47\n100\n"
+var data1 = "นาย ก,74\nนาย ข,99\nนาย ค,5\nนางสาว ง,47\nด.ญ. ฉ,100";
+var ans1 = "74\n99\n5\n47\n100";
 setData(data1, $('[data-question-id=1] .title'), ans1);
+
+var data2 = '<form id="piform" action="submit.php" method="POST">\n	<fieldset>\n		<legend>Personal</legend>\n		<input id="first" type="text"><br>\n		<input id="middlename" type="text"><br>\n		<input type="text" id="last"><br>\n	</fieldset>\n	<input type="number" id="age"><br>\n	<select id="gender">\n		<option value="M">Male</option>\n		<option value="F">Female</option>\n	</select>\n	<div id="responsiveInput" class="col-xs6 colmd-4>\n		<label>ID Number: </label><input id="id">\n		<label>House Type: </label><input id="type">\n	</div>\n</form>';
+var ans2 = "71.5\n87\n99.25\n0\n100";
+setData(data2, $('[data-question-id=2] .title'), ans2);
+
+var data3 = "กมล,ชลวัล,71.5,หญิง,นางสาว\nวัฒนา,87,ชธาธิป,นาย,ชาย\n99.25,นโรโร่ว,ชาย,นาย,นอร์ธ\nSteve,Mr.,ชาย,0,West Gate\nคอร์ ไอ 7,A.I.,เวอร์ชั่น 17.0.01,,100";
+var ans3 = "71.5\n87\n99.25\n0\n100";
+setData(data3, $('[data-question-id=3] .title'), ans3);
+
+var data4 = "กมล,ชลวัล,71.5,หญิง,นางสาว\nวัฒนา,87,ชธาธิป,นาย,ชาย\n99.25,นโรโร่ว,ชาย,นาย,นอร์ธ\nSteve,Mr.,ชาย,0,West Gate\nคอร์ ไอ 7,A.I.,เวอร์ชั่น 17.0.01,,100";
+var ans4 = "71.5\n87\n99.25\n0\n100";
+setData(data4, $('[data-question-id=4] .title'), ans4);
+
+var data5 = "กมล,ชลวัล,71.5,หญิง,นางสาว\nวัฒนา,87,ชธาธิป,นาย,ชาย\n99.25,นโรโร่ว,ชาย,นาย,นอร์ธ\nSteve,Mr.,ชาย,0,West Gate\nคอร์ ไอ 7,A.I.,เวอร์ชั่น 17.0.01,,100";
+var ans5 = "71.5\n87\n99.25\n0\n100";
+setData(data5, $('[data-question-id=5] .title'), ans5);
 
 initInputField($('[data-question-id] .choice-row').css({
 	border: '1px #ffcfa0 solid',
     background: '#ffe8cd'
-}), $('[data-question-id] .title'), [data1], [ans1]);
+}), $('[data-question-id] .title'), [data1,data2,data3,data4,data5], [ans1,ans2,ans3,ans4,ans5]);
 
 function initInputField(divElements, bindQuestionTitles, dataStrings, answerStrings){
 	var inputFieldTemplate = $('<div>').css({
@@ -53,25 +69,41 @@ function setData(data, afterElement, answer, hilight, findRegex, replaceRegex) {
 	var hilightedData = "", hilightedResult = "", currPos = 0;
 	if(hilight){ // For Data -> Hilight Only | For Ans -> Hilight and Replace
 		for(var i in hilight){
-			hilightedData += data.substring(currPos, hilight[i].start);
-			hilightedResult += data.substring(currPos, hilight[i].start);
+			hilightedData += data.substring(currPos, hilight[i].start).replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+			   return '&#'+i.charCodeAt(0)+';';
+			});
+			hilightedResult += data.substring(currPos, hilight[i].start).replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+			   return '&#'+i.charCodeAt(0)+';';
+			});
 			hilightedData += $('<div>').append($('<span>').css({
 				background: '#ffd600',
 			    'box-shadow': 'inset 0px 0px 0px 1px #ff9c00',
 			    'border-radius': '3px'
-			}).text(data.substring(hilight[i].start, hilight[i].start + hilight[i].length))).html();
+			}).html(data.substring(hilight[i].start, hilight[i].start + hilight[i].length).replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+			   return '&#'+i.charCodeAt(0)+';';
+			}))).html();
 			hilightedResult += $('<div>').append($('<span>').css({
 				background: 'rgb(217, 245, 166)',
 			    'box-shadow': 'rgb(139, 195, 33) 0px 0px 0px 1px',
 			    'border-radius': '3px'
-			}).text(data.substring(hilight[i].start, hilight[i].start + hilight[i].length).replace(new RegExp(findRegex), eval('\"' + replaceRegex + '\"')))).html();
+			}).html(data.substring(hilight[i].start, hilight[i].start + hilight[i].length).replace(new RegExp(findRegex), eval('\"' + replaceRegex + '\"')).replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+			   return '&#'+i.charCodeAt(0)+';';
+			}))).html();
 			currPos = hilight[i].start + hilight[i].length;
 		}
-		hilightedData += data.substring(currPos);
-		hilightedResult += data.substring(currPos);
+		hilightedData += data.substring(currPos).replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+		   return '&#'+i.charCodeAt(0)+';';
+		});
+		hilightedResult += data.substring(currPos).replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+		   return '&#'+i.charCodeAt(0)+';';
+		});
 	}else{
-		hilightedData = data;
-		hilightedResult = data;
+		hilightedData = data.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+		   return '&#'+i.charCodeAt(0)+';';
+		});
+		hilightedResult = data.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+		   return '&#'+i.charCodeAt(0)+';';
+		});
 	}
 	// Attach code box with html text
 	var answerCodeBox, resultCodeBox, dataCodeBox;
